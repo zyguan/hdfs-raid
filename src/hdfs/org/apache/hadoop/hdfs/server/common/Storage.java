527184f4fba51b2314da26a34ee56308e31654ae
zyguan <zhongyangguan@gmail.com>
2015-07-09 22:39:15 +0800

    Invoke lockF.deleteOnExit only after lockF has created

--- src/hdfs/org/apache/hadoop/hdfs/server/common/Storage.java
+++ src/hdfs/org/apache/hadoop/hdfs/server/common/Storage.java
@@ -631,7 +631,6 @@ public abstract class Storage extends StorageInfo {
      */
     FileLock tryLock() throws IOException {
       File lockF = new File(root, STORAGE_FILE_LOCK);
-      lockF.deleteOnExit();
       RandomAccessFile file = new RandomAccessFile(lockF, "rws");
       FileLock res = null;
       try {
@@ -644,6 +643,7 @@ public abstract class Storage extends StorageInfo {
         file.close();
         throw e;
       }
+      if (res != null) lockF.deleteOnExit();
       return res;
     }
 
